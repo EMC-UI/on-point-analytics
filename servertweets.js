9ed03880a0fed6005773f38b9296471b74d25299
Isaac Eyeson <Isaac.Eyeson@emc.com>
2016-09-08 10:14:46 -0600

    Refactor #1: make localhost:3000 URL display conference twitter keywords percentages

---
+++ servertweets.js
@@ -0,0 +1,128 @@
+#!/usr/bin/env node
+
+'use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var restClient = require('request-promise');
+var fs = require('file-system');
+
+module.exports = (function() {
+  // todo it would be better to read this in from a file that isn't checked in
+  var consumerKey = 'yYfHaXtb1rHW06TvalYgqHmTc';
+  var consumerSecret = 'OQmDIdT3xRtvVyolAUmuqzCj8xAVrNiskk0LgRrYZ08yqSXHHU';
+  var twitterSearchAPI = 'https://api.twitter.com/1.1/search/tweets.json';
+  var twitterAuthAPI = 'https://api.twitter.com/oauth2/token';
+
+  // this will get updated when /auth is called
+  var twitterAuthToken = 'AAAAAAAAAAAAAAAAAAAAAA9LtwAAAAAAeihb7UwP%2B6hq8GZBlS0HKH%2Bu1qs%3DWAzj5p8Aq67HPskRnFi6ydvN0nogHIyH8Vg4znGo2dDiruNncC';
+  var twitterSearchString = '@VMware OR #VMware OR #VMworld'
+
+
+  var app = express();
+
+  app.use(express.static('.'));
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+  var getHandler = function(req, res) {
+    res.json({
+      "hello": "world"
+    });
+  };
+
+  var putHandler = function(req, res) {
+    console.log('you sent me', req.body);
+    res.status(201).json({
+      "status": "OK"
+    })
+  };
+
+
+  var handleAuth = function(req, res) {
+    console.log('handling auth')
+
+    var combinedString = encodeURIComponent(consumerKey) + ':' + encodeURIComponent(consumerSecret);
+    var b64Encoded = (new Buffer(combinedString)).toString('base64');
+    console.log('b64encoded', b64Encoded)
+
+    var oauthPost = {
+      'method': 'POST',
+      'uri': twitterAuthAPI,
+      'headers': {
+        'Authorization': 'Basic ' + b64Encoded,
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+      },
+      'json': true,
+      'body': 'grant_type=client_credentials'
+    }
+
+    restClient(oauthPost)
+      .then((authResponse) => {
+        console.log('successful response', authResponse)
+
+        // set this variable, which should be used in future calls to
+        twitterAuthToken = authResponse.access_token;
+        res.json({
+          'auth': true,
+          'data': authResponse
+        });
+      })
+      .catch((err) => {
+        console.log('auth error', err);
+        res.status(500).json({
+          'auth': false,
+          'error': err
+        });
+      });
+  }
+
+
+ 
+
+  var getTweets = function(req, res) {
+    let searchTweetsReq = {
+      'request': 'GET',
+      'headers': {
+        'Authorization': 'Bearer ' + twitterAuthToken
+      },
+      'uri': twitterSearchAPI,
+      'qs': {
+        'q': twitterSearchString,
+        'count': 100 
+      },
+      json: true
+    }
+
+    restClient(searchTweetsReq)
+      .then((tweets) => {
+        //console.log(JSON.stringify(tweets, null, '  '))
+        console.log(tweets.statuses.length)
+        for(var i = 0; i < 100; i++){
+           console.log(JSON.stringify(tweets.statuses[0]).replace(/\n|\r\n|\r/g,''));
+           let oneCleanTweet = JSON.stringify(tweets.statuses[0]).replace(/\n|\r\n|\r/g,'')
+           fs.appendFile('tweets-vmware.json', oneCleanTweet + '\n');
+        }
+        res.json(tweets)
+      })
+      .catch((err) => {
+        res.status(500).json({
+          'error': err
+        })
+        console.log('error', err)
+      })
+
+  };
+
+  app.post('/auth', handleAuth);
+  app.get('/foo', getHandler);
+  app.put('/bar', putHandler);
+  app.get('/tweets', getTweets);
+
+  app.listen(5000, function() {
+    console.log('listening on port 5000');
+  });
+
+})()
